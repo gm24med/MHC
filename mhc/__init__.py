6856587f905076dbf03335e9b1da1451316bb8ce
gm24med <simogouali49@gmail.com>
2026-01-10 17:01:48 +0100

    feat: implement deep mlp stress test experiment

--- mhc/__init__.py
+++ mhc/__init__.py
@@ -1,2 +1,3 @@
 from .layers.history_buffer import HistoryBuffer
 from .layers.mhc_skip import MHCSkip
+from .utils.seed import set_seed
---
+++ mhc/utils/__init__.py
@@ -0,0 +1,2 @@
+from .seed import set_seed
+from .tensor_ops import ensure_list, get_last_k
